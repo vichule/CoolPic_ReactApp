57b4c7a018c038136fec86fd1f03126ac2b54053
vichule <javier.cabanasdiaz@gmail.com>
2024-04-24 21:31:38 +0200

    minor changes, new deploy

--- src/features/search/searchThunk.js
+++ src/features/search/searchThunk.js
@@ -2,12 +2,14 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const fetchPics = createAsyncThunk('search/fetchPics', async (queryPicParams) => {
     let url
-    const key = 'acces key';
+    const key = 'tOnZdyE_aVrNGdAMpy-wrX6KVSGzGgpKI8jO3s2Pv2Q';
     const randomPage = Math.floor(Math.random() * 100)
     const picsPage = 12
     if (queryPicParams) {
+        console.log(queryPicParams)
         url = `https://api.unsplash.com/search/photos?query=${queryPicParams}&page=${randomPage}&per_page=${picsPage}&client_id=${key}`;
         console.log('con busqueda')
+        
       } else {
         url = `https://api.unsplash.com/photos/random?client_id=${key}&count=${picsPage}`;
         console.log('sin busqueda')
