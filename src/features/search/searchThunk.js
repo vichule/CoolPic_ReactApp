ea4cc724d7a6292a65af5cc3ce4ab46ae079fc7b
vichule <javier.cabanasdiaz@gmail.com>
2024-06-04 10:08:04 +0200

    gitignore update & env keys

--- src/features/search/searchThunk.js
+++ src/features/search/searchThunk.js
@@ -2,12 +2,11 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const fetchPics = createAsyncThunk('search/fetchPics', async (queryPicParams) => {
   let url;
-  const key = 'AccesKey';
-  const randomPage = Math.floor(Math.random() * 100);
+  const key = import.meta.env.VITE_ACCESS_KEY;
   const picsPage = 12;
 
-// Inicio una cadena de consulta
-let queryParams = ''; 
+  // Inicio una cadena de consulta
+  let queryParams = '';
 
   // Si queryPicParams existe y no está vacío, se convierte en una cadena de consulta y asi evitar el problema al recibir la y mandar la query
   if (queryPicParams) {
@@ -20,11 +19,9 @@ let queryParams = '';
   if (queryPicParams) {
     console.log(queryPicParams);
     url = `https://api.unsplash.com/search/photos?client_id=${key}&query=${queryParams}`;
-    //console.log('URL completa:', url); 
     console.log('con busqueda');
   } else {
     url = `https://api.unsplash.com/photos/random?client_id=${key}&count=${picsPage}`;
-    //console.log('URL completa:', url); 
     console.log('sin busqueda');
   }
 
